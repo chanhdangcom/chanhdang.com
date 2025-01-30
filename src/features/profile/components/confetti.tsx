111b12d35123bec5d2904ccd1215d6767c22215c
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-01-30 22:55:38 +0700

    chore(confetti.tsx): comment out unused code related to confetti display logic to clean up the component and improve readability

--- src/features/profile/components/confetti.tsx
+++ src/features/profile/components/confetti.tsx
@@ -2,37 +2,36 @@
 import React, { useEffect, useState } from "react";
 import { useWindowSize } from "react-use";
 import ReactConfetti from "react-confetti";
-import dayjs from "dayjs";
 
-const saveDate = (today: dayjs.Dayjs) => {
-  const nextConfettiDate = today.add(1, "day").toISOString();
-  localStorage.setItem("confettiDate", nextConfettiDate);
-};
+// const saveDate = (today: dayjs.Dayjs) => {
+//   const nextConfettiDate = today.add(1, "day").toISOString();
+//   localStorage.setItem("confettiDate", nextConfettiDate);
+// };
 
 export const Confetti = () => {
   const [isClient, setIsClient] = useState(false);
   const { width, height } = useWindowSize();
 
-  const [shouldShow, setShouldShow] = useState(false);
+  // const [shouldShow, setShouldShow] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
-  useEffect(() => {
-    const today = dayjs().startOf("date");
-    const confettiDate = localStorage.getItem("confettiDate"); // ISOString | null
+  // useEffect(() => {
+  //   const today = dayjs().startOf("date");
+  //   const confettiDate = localStorage.getItem("confettiDate"); // ISOString | null
 
-    if (
-      !confettiDate ||
-      (confettiDate && dayjs(confettiDate).isBefore(today))
-    ) {
-      saveDate(today);
-      setShouldShow(true);
-    }
-  }, []);
+  //   if (
+  //     !confettiDate ||
+  //     (confettiDate && dayjs(confettiDate).isBefore(today))
+  //   ) {
+  //     saveDate(today);
+  //     setShouldShow(true);
+  //   }
+  // }, []);
 
-  if (!isClient || !shouldShow) return null;
+  if (!isClient) return null;
 
   return <ReactConfetti width={width} height={height} recycle={false} />;
 };
