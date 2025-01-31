e3c5c95ccc7425ea2e295cbf6b756a02795bb65c
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-01-31 08:05:19 +0700

    style(hello.tsx): adjust text size for better responsiveness on medium screens

--- src/features/profile/components/hello.tsx
+++ src/features/profile/components/hello.tsx
@@ -11,7 +11,7 @@ export const Hello = ({ className }: IProps) => {
       <div className="select-none bg-gradient-to-t from-zinc-950 to-zinc-600 bg-clip-text pt-1 font-handwritten text-8xl text-transparent dark:from-zinc-500 dark:to-white">
         Hello
       </div>
-      <div className="font-mono text-2xl dark:text-white">
+      <div className="font-mono text-2xl dark:text-white md:text-xl">
         <span className="font-bold text-sky-400">{"<p>"}</span>I am a Developer.
         <span className="font-bold text-sky-400">{"</p>"}</span>
       </div>
