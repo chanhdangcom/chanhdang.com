ebaeff62487e8f5451423012e64b7e7cfcac6baa
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-02-04 21:57:29 +0700

    feat(TASKS.md): add weather feature details and API endpoint for weather data
    feat(blog): add blog page and page-list components
    
    Add a new blog page component that renders a list of blog posts.
    Implement the PageBlogList component to fetch and display posts from an API.

--- TASKS.md
+++ TASKS.md
@@ -8,3 +8,14 @@ Features:
 - Add Note Item
 - Update Note Item
 Branch: feature/notes
+
+## Weather
+
+API: GET https://api.openweathermap.org/data/2.5/weather?id=1566083&units=metric&appid=a185da73f0467dbc267ce609dda11980
+Features:
+Thoi tiet
+- title: jsonData.weather[0].main
+- description: jsonData.weather[0].description
+- icon: jsonData.weather[0].icon (https://openweathermap.org/img/wn/{iconName}@2x.png)
+Nhiet do
+- jsonData.main.temp (do C)
---
+++ src/features/blog/page-list.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+type IPost = {
+  documentId: string;
+  title: string;
+  description: string;
+  content_blocks: unknown[];
+  slug: string;
+};
+
+// 1. Hoc sau them: Async/Await in JS/TS
+
+// 2. Chuc nang Weather
+
+// const iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+// const iconName = "01n";
+// const iconUrl = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
+
+export const PageBlogList = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetch("https://api.quaric.com/api/articles/custom", {
+  //     method: "GET",
+  //   })
+  //     .then((data) => data.json())
+  //     .then((jsonData) => {
+  //       const posts = jsonData.data as IPost[];
+  //       setPosts(posts);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://api.quaric.com/api/articles/custom", {
+      method: "GET",
+    });
+    const jsonData = await data.json();
+    const posts = jsonData.data as IPost[];
+    setPosts(posts);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1>PageBlogList</h1>
+
+      {isLoading && <div>Loading...</div>}
+
+      {!isLoading && posts.length > 0 && (
+        <div className="border border-red-500">
+          {posts.map((post) => {
+            return (
+              <div key={post.documentId}>
+                <div>{post.title}</div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {!isLoading && !posts.length && <div>No Posts</div>}
+    </div>
+  );
+};
