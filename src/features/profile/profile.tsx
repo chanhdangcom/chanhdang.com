34ceab8447a74fde55191a62e3bc5c17c895fac9
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-02-04 12:44:02 +0700

    style(profile.tsx): adjust padding and gap in profile layout for better responsiveness and visual appeal

--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -13,11 +13,11 @@ export const Profile = () => {
       <div className="relative">
         <div className="absolute left-0 top-8 -z-[1] h-[calc(100vh-120px)] rounded-3xl dark:bg-zinc-900/30 md:w-2/6 xl:bg-zinc-100"></div>
 
-        <div className="container mb-8 space-y-8">
+        <div className="container mb-8 space-y-8 xl:px-16">
           <div className="min-h-[calc(100vh-56px)] items-center space-y-8 md:flex md:space-y-0">
             <Hello className="mt-8 md:hidden" />
 
-            <div className="grid items-center gap-x-16 md:grid-cols-2 xl:gap-x-44">
+            <div className="grid items-center gap-x-16 md:grid-cols-2 xl:gap-x-28">
               <Overview />
               <AboutMe />
             </div>
