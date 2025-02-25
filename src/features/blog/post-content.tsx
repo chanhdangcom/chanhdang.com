98bd9e7a0c40be1e9018ccf50ea10d537a4dd053
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-02-25 13:02:44 +0700

    feat(post-content.tsx): add LoadingBar component to enhance user experience during content loading

--- src/features/blog/post-content.tsx
+++ src/features/blog/post-content.tsx
@@ -6,6 +6,7 @@ import { Footer } from "../profile/footer";
 import { SwitchTheme } from "@/components/switch-theme";
 import { HeaderMotion } from "../profile/components/header-motion";
 import { StrapiBlocksRenderer } from "@/components/strapi-blocks-renderer";
+import { LoadingBar } from "@/components/loading-bar";
 
 type IProps = {
   post: IPost;
@@ -15,22 +16,25 @@ export const PostContent = ({ post }: IProps) => {
   const formattedDate = new Date(post.createdAt).toLocaleDateString("vi-VN");
 
   return (
-    <div className="py-4">
-      <HeaderMotion />
+    <div>
+      <LoadingBar />
+      <div className="py-4">
+        <HeaderMotion />
 
-      <div className="container">
-        <header className="mb-8 flex items-center justify-between text-zinc-400">
-          <div className="text-lg">{formattedDate}</div>
-          <SwitchTheme />
-        </header>
+        <div className="container">
+          <header className="mb-8 flex items-center justify-between text-zinc-400">
+            <div className="text-lg">{formattedDate}</div>
+            <SwitchTheme />
+          </header>
 
-        <div className="prose prose-lg prose-zinc mx-auto mb-8 max-w-3xl dark:prose-invert prose-headings:text-balance prose-img:rounded-lg">
-          <h1>{post.title}</h1>
-          <StrapiBlocksRenderer contentBlocks={post.content_blocks} />
+          <div className="prose prose-lg prose-zinc mx-auto mb-8 max-w-3xl dark:prose-invert prose-headings:text-balance prose-img:rounded-lg">
+            <h1>{post.title}</h1>
+            <StrapiBlocksRenderer contentBlocks={post.content_blocks} />
+          </div>
         </div>
-      </div>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 };
