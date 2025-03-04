da95bf020b18027958673e520378448196b7b39c
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-03-04 20:33:33 +0700

    feat(client.tsx): add Client component to ensure rendering only on client-side
    This change introduces a new Client component that uses a state to determine if the component is being rendered on the client-side. This is necessary for components that rely on client-specific APIs or behaviors.
    
    refactor(photo-list-animate.tsx): wrap AnimatedTestimonials with Client component
    
    Wraps the AnimatedTestimonials component with the new Client component to ensure it only renders on the client-side. This prevents potential issues with server-side rendering and improves performance by avoiding unnecessary rendering on the server.

---
+++ src/components/client.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export function Client({ children }: { children: React.ReactNode }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
