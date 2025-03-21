13e2f7fcb7252835c1e8a021ad6322362e45907a
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-03-21 09:40:39 +0700

    feat(music-time.tsx): add progress bar to display audio playback progress for better user experience refactor(music-time.tsx): calculate progress percentage based on current time and duration for improved readability

--- src/features/profile/components/music-time.tsx
+++ src/features/profile/components/music-time.tsx
@@ -2,12 +2,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import { format } from "date-fns";
 
 import { useAudio } from "@/components/music-provider";
-import { AudioTimeLine } from "@/features/music/component/audio-time-line";
 
 export function MusicTime() {
   const { audioRef, isPlaying } = useAudio();
   const [duration, setDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
+  const progress = duration ? (currentTime / duration) * 100 : 0;
 
   const handleTimeUpdate = useCallback(() => {
     if (audioRef.current) {
@@ -38,7 +38,12 @@ export function MusicTime() {
         {duration ? format(new Date(currentTime * 1000), "m:ss") : "0:00"}
       </div>
 
-      <AudioTimeLine />
+      <div className="mx-auto h-1 w-full overflow-hidden rounded-full bg-zinc-400">
+        <div
+          className="h-full bg-zinc-50 transition-all duration-300"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
 
       <div className="text-xs text-zinc-400">
         {duration ? format(new Date(duration * 1000), "m:ss") : "0:00"}
