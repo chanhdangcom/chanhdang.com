af5e9b5adfb4532b790947b1723f387c69064c8f
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-04-09 14:20:22 +0700

    refactor(music/page.tsx): remove unused state and effect for waveColor to simplify component logic and improve performance

--- src/features/music/page.tsx
+++ src/features/music/page.tsx
@@ -10,45 +10,14 @@ import { TableRanking } from "./table-ranking";
 import { CarouselReplayAudio } from "./carousel-replay-audio";
 import { MusicType } from "./music-type";
 import { SingerList } from "./singer-list";
-import { useState, useEffect } from "react";
-import { FastAverageColor } from "fast-average-color";
-import { useAudio } from "@/components/music-provider";
 
 export function MusicPage() {
-  const [waveColor, setWaveColor] = useState("");
-  const { currentMusic } = useAudio();
-
-  useEffect(() => {
-    if (!currentMusic?.cover) {
-      setWaveColor("rgba(128, 128, 128, 0.6)"); // Xanh
-      return;
-    }
-
-    const fac = new FastAverageColor();
-    fac
-      .getColorAsync(currentMusic?.cover)
-      .then((color) => {
-        const rgbaColor = `rgba(${color.value[0]}, ${color.value[1]}, ${color.value[2]}, 0.8)`;
-        setWaveColor(rgbaColor);
-      })
-      .catch(() => setWaveColor("rgba(128, 128, 128, 0.6)"));
-  }, [currentMusic?.cover]);
-
   return (
     <div className="flex">
       <MenuBar />
 
       <div className="mx-auto w-full">
-        <div
-          className="relative z-10"
-          style={{
-            background: `linear-gradient(to right, #101012, ${waveColor}),
-                         linear-gradient(to bottom, ${waveColor}, rgba(20, 20, 25, 0.9), #101012)`,
-            backdropFilter: "blur(40px)",
-            WebkitBackdropFilter: "blur(40px)",
-            backgroundBlendMode: "multiply",
-          }}
-        >
+        <div className="relative z-10">
           <HeaderMusicPage />
 
           <div className="ml-28 hidden md:flex">
