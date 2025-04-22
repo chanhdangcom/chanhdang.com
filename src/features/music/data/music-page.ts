55a7bfd7f03f54b77837f6795482b4610c2d33b4
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-04-22 09:43:35 +0700

    feat(music-page.ts): add new music entry for "Tháp Drill Tự Do" by Ê Kê Vin to enrich the music data collection

--- src/features/music/data/music-page.ts
+++ src/features/music/data/music-page.ts
@@ -380,6 +380,19 @@ mang phong cách đáng yêu, trẻ trung.
   },
 
   {
+    id: "7ff65b56-3fd3-4814-9fea-9f722b4250d7231223r3221",
+    title: "Tháp Drill Tự Do",
+    singer: "Ê Kê Vin",
+    cover: "https://cdn.chanhdang.com/beauty_singer_khanh_vin.jpg",
+    audio: "https://cdn.chanhdang.com/music_thap_drill_tu_do.mp3",
+    youtube: "https://www.youtube.com/watch?v=Zt7eyyAIEDw",
+    content: `
+
+    `,
+    type: "Jack"
+  },
+
+  {
     id: "7ff65b56-3fd3-4814-9fea-9f722b4250d72321",
     title: "BÔNG LAN",
     singer: "Minh Vương & Lệ Thuỷ",
