67b74ec5afa6c2d71435789cb9bd881d08135097
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-05-31 17:04:24 +0700

    feat(helper): add helper images and components for user guidance in the application feat(helper): implement helper page with dynamic content and interactive elements for better user experience feat(ui): create Lens, PointerHighlight, and TracingBeam components for enhanced visual effects and interactions

---
+++ src/app/helper/page.tsx
@@ -0,0 +1,5 @@
+import { HelperPage } from "@/features/helper/helper-page";
+
+export default function Page() {
+  return <HelperPage />;
+}
---
+++ src/features/helper/helper-page.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React, { useState } from "react";
+import { TracingBeam } from "@/components/ui/tracing-beam";
+import { Lens } from "@/components/ui/lens";
+import { motion } from "motion/react";
+import { PointerHighlight } from "@/components/ui/pointer-highlight";
+import { Confetti } from "../profile/components/confetti";
+import { Footer } from "../profile/footer";
+
+export function HelperPage() {
+  const [hovering, setHovering] = useState(false);
+
+  return (
+    <div>
+      <Confetti />
+      <TracingBeam className="px-6">
+        <div className="mx-auto mt-16 max-w-lg text-2xl tracking-tight md:text-6xl">
+          <div className="text-5xl">Hướng dẫn sử dụng</div>
+          <PointerHighlight>
+            <span className="font-bold text-blue-500">Quản lí bán coffee</span>
+          </PointerHighlight>
+        </div>
+
+        <div className="relative mx-auto max-w-6xl pt-4 antialiased">
+          {dummyContent.map((item, index) => (
+            <div key={`content-${index}`} className="mb-10">
+              <>
+                {item?.image && (
+                  <div className="relative mx-auto my-10 max-w-6xl overflow-hidden rounded-3xl bg-gradient-to-r from-[#1D2235] to-[#121318] p-8">
+                    <div className="relative z-10">
+                      <Lens hovering={hovering} setHovering={setHovering}>
+                        <img
+                          src={item.image}
+                          alt="blog thumbnail"
+                          height="5000"
+                          width="5000"
+                          className="mb-10 h-auto max-w-full rounded-lg object-cover"
+                        />
+                      </Lens>
+
+                      <motion.div
+                        animate={{
+                          filter: hovering ? "blur(2px)" : "blur(0px)",
+                        }}
+                        className="relative z-20 py-4"
+                      >
+                        <h2 className="text-left text-2xl font-bold text-white">
+                          {item.title}
+                        </h2>
+
+                        <p className="mt-4 text-left text-neutral-200">
+                          {item.description}
+                        </p>
+                      </motion.div>
+                    </div>
+                  </div>
+                )}
+              </>
+            </div>
+          ))}
+        </div>
+        <div className="mb-16">
+          <Footer />
+        </div>
+      </TracingBeam>
+    </div>
+  );
+}
+
+const dummyContent = [
+  {
+    title: "Đăng nhập",
+    description: (
+      <>
+        <p>
+          Cho phép người dùng (Quản trị viên hoặc Nhân viên) truy cập vào hệ
+          thống bằng cách cung cấp tên đăng nhập và mật khẩu hợp lệ. Hệ thống sẽ
+          xác thực thông tin và chuyển hướng người dùng đến giao diện chính phù
+          hợp với quyền hạn của họ.
+        </p>
+      </>
+    ),
+    badge: "UI",
+    image: "/img/helper/DangNhap.jpg",
+  },
+  {
+    title: "Quản lí từng bàn",
+    description: (
+      <>
+        <p>
+          Cho phép nhân viên hoặc quản trị viên chọn một bàn cụ thể từ danh sách
+          các bàn có sẵn để bắt đầu hoặc tiếp tục một order. Trạng thái của bàn
+          (phục vụ, trống, đang phục vụ) được hiển thị trực quan.
+        </p>
+      </>
+    ),
+    badge: "UI",
+    image: "/img/helper/QLBanHang.jpg",
+  },
+  {
+    title: "Quản lí bàn",
+    description: (
+      <>
+        <p>
+          Cho phép nhân viên hoặc quản trị viên chọn một bàn cụ thể từ danh sách
+          các bàn có sẵn để bắt đầu hoặc tiếp tục một order. Trạng thái của bàn
+          (phục vụ, trống, đang phục vụ) được hiển thị trực quan.
+        </p>
+      </>
+    ),
+    badge: "UI",
+    image: "/img/helper/QLBan.jpg",
+  },
+
+  {
+    title: "Quản thức uống",
+    description: (
+      <>
+        <p>
+          Cho phép nhân viên hoặc quản trị viên chọn một bàn cụ thể từ danh sách
+          các bàn có sẵn để bắt đầu hoặc tiếp tục một order. Trạng thái của bàn
+          (phục vụ, trống, đang phục vụ) được hiển thị trực quan.
+        </p>
+      </>
+    ),
+    badge: "UIg",
+    image: "/img/helper/QLThucUong.jpg",
+  },
+];
