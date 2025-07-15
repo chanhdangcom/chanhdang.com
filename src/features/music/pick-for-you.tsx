6b9bb82666375d4c6fc098cce18beb3f2c92df2a
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-07-15 23:00:28 +0700

    feat(pick-for-you.tsx): enhance UI with additional audio mix cards for better user engagement refactor(profile.tsx): clean up layout by removing unused components and improving container alignment

--- src/features/music/pick-for-you.tsx
+++ src/features/music/pick-for-you.tsx
@@ -33,7 +33,45 @@ export function PickForYou() {
 
         <div
           onClick={() => handlePlayRandomAudio()}
-          className="animate-bg-gradient h-72 w-56 shrink-0 rounded-xl bg-gradient-to-br from-[#8E2DE2] via-[#4A00E0] to-[#00C9FF] bg-[length:400%_400%] text-white"
+          className="animate-bg-gradient h-72 w-56 shrink-0 rounded-xl bg-gradient-to-tr from-[#ff6fd8] via-[#3813c2] to-[#4fd1c5] bg-[length:200%_200%]"
+        >
+          <div className="flex h-full flex-col justify-between">
+            <div className="p-4">
+              <ChanhdangLogotype className="mb-4 w-24" />
+              <div className="text-4xl font-bold">Get Up!</div>
+              <div className="text-3xl">Mix</div>
+            </div>
+
+            <div className="rounded-b-xl bg-zinc-700/20 px-4 py-2">
+              <div className="text-center">
+                Ê Kê Vin, Khầy, Jack - J97 and more
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          onClick={() => handlePlayRandomAudio()}
+          className="animate-bg-gradient h-72 w-56 shrink-0 rounded-xl bg-gradient-to-br from-[#fbd3e9] via-[#bb377d] to-[#fbd3e9] bg-[length:400%_400%] text-white"
+        >
+          <div className="flex h-full flex-col justify-between">
+            <div className="p-4">
+              <ChanhdangLogotype className="mb-4 w-24" />
+              <div className="text-4xl font-bold">New M</div>
+              <div className="text-3xl">Mix</div>
+            </div>
+
+            <div className="rounded-b-xl bg-zinc-700/20 px-4 py-2">
+              <div className="line-clamp-2 text-center">
+                Ê Kê Vin, Khầy, Jack - J97, Vũ , Son Tung M-TP and more
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          onClick={() => handlePlayRandomAudio()}
+          className="animate-bg-gradient h-72 w-56 shrink-0 rounded-xl bg-gradient-to-br from-[#f83600] via-[#f9d423] to-[#f83600] bg-[length:400%_400%] text-white"
         >
           <div className="flex h-full flex-col justify-between">
             <div className="p-4">
--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -7,7 +7,6 @@ import { Confetti } from "./components/confetti";
 import { Footer } from "./footer";
 
 import { SectionBlog } from "./section-blog";
-import { PhotoListAnimate } from "./photo-list-animate";
 
 import { ScrollHeaderPage } from "@/components/scroll-header-page";
 import { HeaderMotion } from "./components/header-motion";
@@ -16,12 +15,12 @@ import { MusicList } from "./music-list";
 
 export const Profile = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center">
       <Header />
       <HeaderMotion />
-      {/* <HeaderMotionGlass /> */}
-      <div className="relative">
-        <div className="absolute left-0 top-8 -z-[1] h-[calc(100vh-120px)] rounded-3xl dark:bg-zinc-900/30 md:w-2/6 xl:bg-zinc-100"></div>
+
+      <div className="relative w-full">
+        {/* <div className="absolute left-0 top-8 -z-[1] h-[calc(100vh-120px)] rounded-3xl dark:bg-zinc-900/30 md:w-2/6 xl:bg-zinc-100"></div> */}
         <Hello className="my-8 md:hidden" />
 
         <WavyBackground className="mx-auto max-w-full">
@@ -35,23 +34,23 @@ export const Profile = () => {
           </div>
         </WavyBackground>
 
-        <div className="container my-8 justify-center md:flex md:items-center">
+        {/* <div className="container my-8 md:flex md:items-center">
           <PhotoListAnimate />
-        </div>
+        </div> */}
 
-        <div className="container max-w-4xl">
+        <div className="container mx-auto max-w-4xl">
           <ExperienceList />
         </div>
 
-        <div className="container relative my-8 max-w-3xl">
+        <div className="container relative mx-auto my-8 max-w-3xl">
           <MusicList />
         </div>
 
-        <div className="container my-8 max-w-4xl">
+        <div className="container mx-auto my-8 max-w-4xl">
           <SectionBlog />
         </div>
 
-        <div className="container my-8">
+        <div className="container mx-auto my-8">
           <Footer />
         </div>
       </div>
@@ -59,7 +58,8 @@ export const Profile = () => {
       <div className="m-8">
         <ScrollHeaderPage />
       </div>
+
       <Confetti />
-    </>
+    </div>
   );
 };
