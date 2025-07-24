5f5dc3d3d7d4e3c50a846f8252a93e6eeb0e7d11
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-07-24 21:08:01 +0700

    fix(mongodb.ts): declare global variable for MongoClient promise to avoid multiple connections in development chore(mongodb.ts): add comment to clarify the purpose of the MONGODB_URI environment variable

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,12 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string; // Đặt biến môi trường này trong .env.local
+// Khai báo mở rộng type cho global
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+const uri = process.env.MONGODB_URI as string;
 const options = {};
 
 let client;
@@ -11,13 +17,10 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === "development") {
-
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-
     global._mongoClientPromise = client.connect();
   }
-
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
