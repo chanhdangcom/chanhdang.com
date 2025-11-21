43ff3ff49a02412abdb267829b9f9c5b04d3896c
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-11-21 19:09:37 +0700

    feat(header-motion.tsx): replace motion.svg with Image component for better performance and accessibility style(header.tsx): adjust spacing in navigation items for improved layout consistency style(globals.css): change dark background color from #030610 to black for better contrast and readability

--- src/app/[locale]/features/profile /header.tsx
+++ src/app/[locale]/features/profile /header.tsx
@@ -37,58 +37,70 @@ export const Header = () => {
           <ChanhdangLogotype />
         </Link>
 
-        <nav aria-label="Primary" className="hidden md:block">
-          <ul className="flex items-center gap-6 font-mono text-sm">
-            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-              <Link href="/" className="hover:underline">
-                Daifolio
-              </Link>
-            </motion.li>
+        <div className="flex items-center gap-8">
+          <nav aria-label="Primary" className="hidden md:block">
+            <ul className="flex items-center gap-6 font-mono text-sm">
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Link href="/" className="hover:underline">
+                  Daifolio
+                </Link>
+              </motion.li>
 
-            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-              <Link href="/music" className="hover:underline">
-                Musics
-              </Link>
-            </motion.li>
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Link href="/music" className="hover:underline">
+                  Musics
+                </Link>
+              </motion.li>
 
-            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-              <Link href="/blog" className="hover:underline">
-                Blogs
-              </Link>
-            </motion.li>
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Link href="/blog" className="hover:underline">
+                  Blogs
+                </Link>
+              </motion.li>
 
-            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-              <Link href="/components" className="hover:underline">
-                Components
-              </Link>
-            </motion.li>
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Link href="/components" className="hover:underline">
+                  Components
+                </Link>
+              </motion.li>
 
-            <li>
-              <ChatBox />
-            </li>
-          </ul>
-        </nav>
+              <li>
+                <ChatBox />
+              </li>
+            </ul>
+          </nav>
 
-        <div className="flex items-center gap-2">
-          <ThemeToggle />
+          <div className="flex items-center gap-2">
+            <ThemeToggle />
 
-          {/* <SwitchLanguage /> */}
-
-          <Link
-            href="https://github.com/chanhdangcom"
-            target="_blank"
-            className="rounded-full border border-zinc-300 p-1 dark:border-zinc-800"
-          >
-            <img
-              src={
-                theme === "dark"
-                  ? "/img/tech-stack/github-mark-white.svg"
-                  : "/img/tech-stack/github-mark.svg"
-              }
-              alt="icon"
-              className="size-6"
-            />
-          </Link>
+            <Link
+              href="https://github.com/chanhdangcom"
+              target="_blank"
+              className="rounded-full border border-zinc-300 p-1 dark:border-zinc-800"
+            >
+              <img
+                src={
+                  theme === "dark"
+                    ? "/img/tech-stack/github-mark-white.svg"
+                    : "/img/tech-stack/github-mark.svg"
+                }
+                alt="icon"
+                className="size-6"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </>
