d591e9980d4f91417d0da9dff80120837daf7354
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-12-30 09:43:11 +0700

    style(page.tsx): adjust left and right fixed div positions to use viewport width for better responsiveness style(playlist-detail-client.tsx): disable Next.js image element linting rule to allow usage of img tag without src set

--- src/app/[locale]/components/page.tsx
+++ src/app/[locale]/components/page.tsx
@@ -92,17 +92,15 @@ export default function Page() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
       />
       <div>
-        <div className="fixed left-0 h-screen w-px bg-zinc-200 dark:bg-zinc-900 md:left-48" />
-        <div className="fixed right-0 h-screen w-px bg-zinc-200 dark:bg-zinc-900 md:right-48" />
-
-        <div className="mx-0 border border-zinc-300 p-2 dark:border-zinc-900 md:mx-48">
+        <div className="fixed left-0 h-screen w-px bg-zinc-200 dark:bg-zinc-900 md:left-[15vw]" />
+        <div className="fixed right-0 h-screen w-px bg-zinc-200 dark:bg-zinc-900 md:right-[15vw]" />
+        <div className="mx-0 border border-zinc-300 p-2 dark:border-zinc-900 md:mx-[15vw]">
           <Header />
         </div>
 
         <div className="mt-6">
           <ComponentList />
         </div>
-
         <Footer />
       </div>
     </>
